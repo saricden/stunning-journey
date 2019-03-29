dadb8ae0e68adc5d9b019c23b6ff49b3ace915b6
Pedro Rodrigues <medecau@gmail.com>
2019-03-29 07:25:28 +0000

    adds Scene1

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import style from './main.css';
 import Phaser, {Game} from 'phaser';
 import PreloaderScene from './scenes/PreloaderScene';
+import Scene1 from './scenes/Scene1';
 
 const canvas = document.getElementById('game-canvas');
 
@@ -17,8 +18,10 @@ const config = {
     }
   },
   scene: [
+    Scene1,
     PreloaderScene
   ]
 };
 
 const game = new Game(config);
+game.state = {}
---
+++ src/scenes/Scene1.js
@@ -0,0 +1,14 @@
+import {Scene} from 'phaser';
+
+export default class Scene1 extends Scene {
+  constructor() {
+    super("scene1");
+  }
+
+  create() {
+  	console.log('creating Scene1')
+    
+  }
+}
+
+
