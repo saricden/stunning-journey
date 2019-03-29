681580f021844a0f4ed8921f4989ed2404ae66ea
Kirk M <kirk@saricden.com>
2019-03-29 01:13:25 -0700

    Adds some brick islands.

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -11,6 +11,18 @@ export default class Scene1 extends Scene {
     
   	console.log('creating Scene1')
     const hero = this.add.sprite(50, 50, 'hero') 
+
+    // Init our brick group
+    this.bricks = this.physics.add.staticGroup();
+
+    // Draw some brick islands
+    for (let y = 200; y < 1000; y += 200) {
+      const xOffset = ((y * 2) - 400);
+      for (let x = 0; x < 400; x += 100) {
+
+        this.bricks.create((xOffset + x), y, 'tile-brick');
+      }
+    }
   }
 }
 
