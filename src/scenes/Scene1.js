af6191b51c782bd78a5197e6cc50b7b6150d94d6
Kirk M <kirk@saricden.com>
2019-03-29 01:46:57 -0700

    Adds some basic Arcade physics! :shipit:

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -11,8 +11,24 @@ export default class Scene1 extends Scene {
     
   	console.log('creating Scene1')
     this.state = {}
-    this.state.hero = this.add.sprite(50, 50, 'hero')
+    this.state.hero = this.physics.add.sprite(50, 50, 'hero')
     this.state.cursors = this.input.keyboard.createCursorKeys();
+
+    // Init our brick group
+    this.bricks = this.physics.add.staticGroup();
+
+    // Draw some brick islands
+    for (let y = 200; y < 1000; y += 200) {
+      const xOffset = ((y * 2) - 400);
+      for (let x = 0; x < 400; x += 100) {
+
+        this.bricks.create((xOffset + x), y, 'tile-brick');
+      }
+    }
+
+    // Map collider
+    this.physics.add.collider(this.state.hero, this.bricks);
+
 
     // Follow the character
     this.cameras.main.startFollow(this.state.hero);
@@ -30,18 +46,6 @@ export default class Scene1 extends Scene {
       ls.hero.y -= gs.speed}
     if (ls.cursors.down.isDown){
       ls.hero.y += gs.speed}
-
-    // Init our brick group
-    this.bricks = this.physics.add.staticGroup();
-
-    // Draw some brick islands
-    for (let y = 200; y < 1000; y += 200) {
-      const xOffset = ((y * 2) - 400);
-      for (let x = 0; x < 400; x += 100) {
-
-        this.bricks.create((xOffset + x), y, 'tile-brick');
-      }
-    }
   }
 }
 
